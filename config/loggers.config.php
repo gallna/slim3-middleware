5486cee16968e522080534b8011e5efa42588c1c
Tomasz Jonik <tom.jonik@hotmail.com>
2016-08-14 14:12:29 +0100

    Added Monolog gelf-handler

--- config/loggers.config.php
+++ config/loggers.config.php
@@ -2,12 +2,40 @@
 use Pimple\Container;
 
 return [
+    "gelf-host" => getenv("GRAYLOG_HOST"),
 
-    "logger" => function() {
+    "gelf-port" => getenv("GRAYLOG_PORT"),
+
+    "logger" => function(Container $container) {
         $logger = new Monolog\Logger(getenv("PROJECT_NAME"));
-        $logger->pushHandler($handler = new Monolog\Handler\ErrorLogHandler());
-        // $logger->warn("Some warning");
+        if (isset($container["amqp-broker"])) {
+            $logger->pushHandler($container["gelf-amqp-handler"]);
+        } elseif($container["gelf-host"] && $container["gelf-port"]) {
+            $logger->pushHandler($container["gelf-tcp-handler"]);
+        } else {
+            $logger->pushHandler($container["error-log-handler"]);
+        }
         return $logger;
+    },
+
+    "error-log-handler" => function(Container $container) {
+        return new Monolog\Handler\ErrorLogHandler();
+    },
+
+    "gelf-tcp-handler" => function(Container $container) {
+        $transport = new Gelf\Transport\TcpTransport("graylog.graylog.stream.weeb.online", 12203);
+        $publisher = new Gelf\Publisher();
+        $publisher->addTransport($transport);
+        return new Monolog\Handler\GelfHandler($publisher);
+    },
+
+    "gelf-amqp-handler" => function(Container $container) {
+        $broker = $container["amqpBroker"];
+        $exchange = $broker->declareExchange(AMQP_DURABLE, 'log-messages', AMQP_EX_TYPE_FANOUT);
+        $queue = $broker->queue(null, "log-messages");
+        $publisher = new Gelf\Publisher();
+        $publisher->addTransport(new Gelf\Transport\AmqpTransport($exchange, $queue));
+        return new Monolog\Handler\GelfHandler($publisher);
     },
 
     "cliFormatter" => function(Container $container) {
